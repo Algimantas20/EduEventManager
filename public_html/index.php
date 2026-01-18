7ace062c21ff2d74c0917e502b6c31a8d748b446
KTUMVG0800 <algimantas.samalionis@jkm.lt>
2026-01-18 10:04:29 +0200

    Added basic HTML  code to index.php

---
+++ public_html/index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>EduEventManager</title>
+</head>
+<body>
+    <main>
+        <h1>EduEventManager</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam ratione ipsam doloribus. Architecto laboriosam odio dolores nisi perferendis aperiam ad dignissimos excepturi libero. Praesentium quis ad officiis voluptatem excepturi magni.
+    </main>
+</body>
+</html>
