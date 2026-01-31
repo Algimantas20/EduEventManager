1a2e64792f8db1f66c4f8067b0c5e375ab3058b1
Algimantas20 <algsamalionis@gmail.com>
2026-01-31 16:58:25 +0200

    Cleaned up the table css

--- public_html/index.php
+++ public_html/index.php
@@ -2,9 +2,13 @@
 
 <html>
 <head>
-    <title>EduEventManager</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+
     <link rel="stylesheet" href="styles/components/header.css">
     <link rel="stylesheet" href="styles/index.css">
+
+    <title>EduEventManager</title>
 </head>
 <body>
     <?= renderPageHeader('Home') ?>
--- public_html/students.php
+++ public_html/students.php
@@ -32,12 +32,15 @@ $db->disconnect($conn);
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
-    <title>EduEventManager - Students</title>
+    <meta charset="UTF-8">
 
     <link rel="stylesheet" href="styles/components/header.css">
     <link rel="stylesheet" href="styles/components/table.css">
     <link rel="stylesheet" href="styles/students.css">
+    
+    <title>EduEventManager - Students</title>
 </head>
 <body>
 
